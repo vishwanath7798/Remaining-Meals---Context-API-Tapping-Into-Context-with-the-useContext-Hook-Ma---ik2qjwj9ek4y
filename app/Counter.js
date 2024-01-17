292c2354319180791d51b573441f796a45d5b9e9
vishwanath s patil <126999436+vishwanath7798@users.noreply.github.com>
2024-01-17 11:50:04 +0530

    Update Counter.js

--- app/Counter.js
+++ app/Counter.js
@@ -1,11 +1,13 @@
 'use client'
-import React from "react";
+import React, { useContext } from "react";
+import { MealsContext } from "./MealsProvider";
 
 const Counter = () => {
-
+    const { meals } = useContext(MealsContext);
+    const remaingMeals = meals.filter((meal) => !meal.ticked).length;
     return (
         <div>
-            <h3>Meals Remaining: remaingMeals</h3>
+            <h3>Meals Remaining: {remaingMeals}</h3>
         </div>
     )
 }
